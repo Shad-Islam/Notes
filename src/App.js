e301bc0279d22172f644a6c88f3f7b94bd411eb0
Shad-islam <shadislam48@gmail.com>
2023-10-28 02:10:20 +0600

    Split app.js and state lifting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import { useState } from "react";
+import NoteForm from "./component/NoteForm";
+import NoteList from "./component/NoteList";
 
 function App() {
   const [noteTitle, setNotetitle] = useState("");
@@ -7,86 +9,24 @@ function App() {
   const [edit, setEdit] = useState(false);
   const [update, setUpdate] = useState(null);
 
-  // create handler
-  const createHandler = (event) => {
-    event.preventDefault();
-    if (!noteTitle) {
-      return alert("Please Enter Note Title");
-    }
-    const newNote = {
-      id: Date.now() + "",
-      title: noteTitle,
-    };
-    setNotes([...notes, newNote]);
-    setNotetitle("");
-  };
-
-  //remove button
-  const removeHandler = (noteId) => {
-    const newNotes = notes.filter((note) => note.id !== noteId);
-    setNotes(newNotes);
-  };
-
-  // edit button
-  const editHandler = (note) => {
-    setEdit(true);
-    setNotetitle(note.title);
-    setUpdate(note);
-  };
-
-  const updateHandler = (event) => {
-    event.preventDefault();
-    const updatedNoteArray = notes.map((note) => {
-      if (note.id === update.id) {
-        return { ...note, title: noteTitle };
-      } else {
-        return note;
-      }
-    });
-    setNotes(updatedNoteArray);
-    setNotetitle("");
-    setEdit(false);
-  };
-
   return (
     <div className="container">
-      <form onSubmit={edit ? updateHandler : createHandler} className="form">
-        <input
-          type="text"
-          className="input-field"
-          value={noteTitle}
-          onChange={(event) => {
-            setNotetitle(event.target.value);
-          }}
-        />
-
-        <button type="submit" className="submit-btn">
-          {edit ? "Update Note" : "Add Note"}
-        </button>
-      </form>
-      <ul className="note-list">
-        {notes.map((note) => (
-          <li key={note.id} className="note-box">
-            <span>{note.title}</span>
-            <div className="note-btn">
-              <button
-                onClick={() => {
-                  editHandler(note);
-                }}
-              >
-                Edit
-              </button>
-              <button
-                onClick={() => {
-                  removeHandler(note.id);
-                }}
-              >
-                Remove
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <NoteForm
+        edit={edit}
+        notes={notes}
+        update={update}
+        setEdit={setEdit}
+        setNotes={setNotes}
+        noteTitle={noteTitle}
+        setNotetitle={setNotetitle}
+      />
+      <NoteList
+        notes={notes}
+        setEdit={setEdit}
+        setNotes={setNotes}
+        setUpdate={setUpdate}
+        setNotetitle={setNotetitle}
+      />
     </div>
   );
 }
