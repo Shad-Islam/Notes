8d743723d76f83ab18fdae95ae67483f8a2b1cf9
Shad-islam <shadislam48@gmail.com>
2023-11-09 14:45:47 +0600

    Replace all the state

--- src/context/NoteProvider.jsx
+++ src/context/NoteProvider.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { useState } from "react";
 import { createContext } from "react";
 
 export const NoteContext = createContext();
 
 function NoteProvider({ children }) {
+  const [noteTitle, setNotetitle] = useState("");
+  const [notes, setNotes] = useState([]);
+  const [edit, setEdit] = useState(false);
+  const [update, setUpdate] = useState(null);
+
+  const NoteContextValue = {
+    edit,
+    notes,
+    update,
+    setEdit,
+    setNotes,
+    setUpdate,
+    noteTitle,
+    setNotetitle,
+  };
   return (
     <div>
-      <NoteContext.Provider>{children}</NoteContext.Provider>
+      <NoteContext.Provider value={NoteContextValue}>
+        {children}
+      </NoteContext.Provider>
     </div>
   );
 }
