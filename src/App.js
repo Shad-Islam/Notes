96d24c0e34bcecd40de79d683f6777a00beb7099
Shad-islam <shadislam48@gmail.com>
2023-11-09 17:51:18 +0600

    Clean code

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import "./App.css";
-// import { useState } from "react";
 import NoteForm from "./component/NoteForm";
 import NoteList from "./component/NoteList";
 
 function App() {
-  // const [noteTitle, setNotetitle] = useState("");
-  // const [notes, setNotes] = useState([]);
-  // const [edit, setEdit] = useState(false);
-  // const [update, setUpdate] = useState(null);
-
   return (
     <div className="container">
-      <NoteForm
-      // edit={edit}
-      // notes={notes}
-      // update={update}
-      // setEdit={setEdit}
-      // setNotes={setNotes}
-      // noteTitle={noteTitle}
-      // setNotetitle={setNotetitle}
-      />
-      <NoteList
-      // notes={notes}
-      // setEdit={setEdit}
-      // setNotes={setNotes}
-      // setUpdate={setUpdate}
-      // setNotetitle={setNotetitle}
-      />
+      <NoteForm />
+      <NoteList />
     </div>
   );
 }
